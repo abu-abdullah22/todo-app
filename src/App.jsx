381e7dbf14b5f60e68ac08b58e1da84a538f08c5
abu-abdullah22 <rorschachmayhem@gmail.com>
2024-12-30 21:04:06 +0600

    rendering the components in the app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import Header from "./components/Header";
+import Tabs from "./components/Tabs";
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
 
 function App() {
-  
-let x = 3 ;
+
   return (
     <>
-    {x}
       <Header />
+      <Tabs />
+      <TodoList />
+      <TodoInput />
     </>
   )
 }
