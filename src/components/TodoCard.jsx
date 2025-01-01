a56c9898af6c7ff363b9f712a454ac90ffefd182
abu-abdullah22 <rorschachmayhem@gmail.com>
2025-01-01 23:59:02 +0600

    card added basic

--- src/components/TodoCard.jsx
+++ src/components/TodoCard.jsx
@@ -4,7 +4,17 @@ const TodoCard = ({todoIndex, todos}) => {
   
     
     return (
-        <div></div>
+        <div className="card todo-item">
+            <p>{todo.input}</p>
+            <div className="todo-button">
+                <button>
+                    <h6>Done</h6>
+                </button>
+                <button>
+                    <h6>Delete</h6>
+                </button>
+            </div>
+        </div>
     );
 };
 
