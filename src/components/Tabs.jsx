02cd5a79db85d12790b83f9311dc8e3aaf01bbb2
abu-abdullah22 <rorschachmayhem@gmail.com>
2025-01-01 23:18:12 +0600

    tabs condition rendering done

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,12 +1,20 @@
-const Tabs = () => {
-    
-    const tabs = ['All', 'Open', 'Completed'] ;
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+const Tabs = ({ todos }) => {
+
+
+
+    const tabs = ['All', 'Open', 'Completed'];
 
     return (
         <nav className="tab-container">
-            {tabs.map((tab, tabIndex)=> {
+            {tabs.map((tab, tabIndex) => {
+                const numOfTasks = tab === 'All' ? todos.length :
+                tab === 'Open' ? todos.filter(val => !val.complete).length : 
+                todos.filter(val => val.complete).length ;
+
                 return (
-                    <button className="tab-button" key={tabIndex}><h4>{tab}(0)</h4></button>
+                    <button className="tab-button" key={tabIndex}><h4>{tab} <span>{numOfTasks}</span></h4></button>
                 )
             })}
         </nav>
