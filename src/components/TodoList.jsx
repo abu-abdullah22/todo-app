e15ec444a77aff59ad175c379fe6dca476b121c3
abu-abdullah22 <rorschachmayhem@gmail.com>
2025-01-02 21:29:19 +0600

    creating filtredtodolist for tabs

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -4,18 +4,25 @@ import TodoCard from "./TodoCard";
 
 /* eslint-disable react/prop-types */
 
-const TodoList = ({todos}) => {
+const TodoList = ({ todos }) => {
 
-    const tab = 'All' ;
-    return (
-        <>
-          {todos.map((todo, todoIndex)=>{
-            return (
-                <TodoCard key={todoIndex} todoIndex={todoIndex} todos={todos}/>
-            )
-          })}  
-        </>
-    );
+  const tab = 'All';
+  const filteredTodoList = tab === 'All' ?
+    todos :
+    tab === 'Completed' ?
+      todos.filter(val => val.complete) :
+      todos.filter(val => !val.complete);
+
+
+  return (
+    <>
+      {todos.map((todo, todoIndex) => {
+        return (
+          <TodoCard key={todoIndex} todoIndex={todoIndex} todos={todos} />
+        )
+      })}
+    </>
+  );
 };
 
 export default TodoList;
